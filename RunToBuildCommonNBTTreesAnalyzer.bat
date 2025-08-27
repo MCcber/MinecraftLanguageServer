b16ea8dc81182eeb2014b35aaa3a06906e88bec3
洪荒太初_cber <2682265891@qq.com>
2025-08-27 19:16:14 +0800

    三个必要的批处理文件

---
+++ RunToBuildCommonNBTTreesAnalyzer.bat
@@ -0,0 +1,2 @@
+java -jar antlr-4.13.1-complete.jar "E:\C#Projects\AutoComplete\Grammar\commonNBTTrees.g4"
+pause
---
+++ RunToBuildSelectorsAnalyzer.bat
@@ -0,0 +1,2 @@
+java -jar antlr-4.13.1-complete.jar "E:\C#Projects\AutoComplete\Grammar\selector.g4"
+pause
